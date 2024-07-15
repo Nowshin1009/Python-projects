51ccd13e2524d493353d25b6d9ec026413ecde1a
nowshin1009 <nowshin35-1009@diu.edu.bd>
2024-07-15 16:53:17 +0600

    update contact

--- Contact Book/App.py
+++ Contact Book/App.py
@@ -71,6 +71,46 @@ def remove_contact():
         contact_Book.pop(selected_index-1)
         print("Contact removed successfully!")
 
+
+def update_contact():
+    #select contact --> get new values --> replace with old value
+    found_search_result=False
+    search_term = input("Enter text to search to update : ")
+    for index, contact in enumerate(contact_Book):
+        if search_term.lower() in contact["name"].lower():
+            found_search_result=True
+            print(f"{index+1}. {contact['name']} - {contact['phone']}")
+
+        if not found_search_result:
+            print("No such contact")
+            return
+    selected_index = input("Enter an contact to update: ")
+    selected_index= int(selected_index)
+    
+    new_name=input("Enter new name: ")
+    new_phone=input("Enter new phone number: ")
+    new_email=input("Enter new email: ")
+
+    # sequentially -->
+    # contact_Book[selected_index - 1]["name"]=new_name
+    # contact_Book[selected_index - 1]["phone"]=new_phone
+    # contact_Book[selected_index - 1]["email"]=new_email
+
+
+    # with mathod -->
+    contact_Book[selected_index-1].update(
+        {
+            "name":new_name,
+            "phone":new_phone,
+            "email":new_email
+        }
+    )
+    print("Contact updated successfully!!!")
+
+
+    
+
+
 print("=======WELCOME=========")
 menu_text= """
 Your options:
@@ -78,7 +118,8 @@ Your options:
 2. View All contacts
 3. Search a contact
 4. Remove a contact
-5. Exit
+5. Update a contact
+0. Exit
 """
 
 while True:
@@ -97,6 +138,8 @@ while True:
     elif choice == "4":
         remove_contact()
     elif choice == "5":
+        update_contact()
+    elif choice == "0":
         break
     else:
         print("Wrong choice!! Try again")
